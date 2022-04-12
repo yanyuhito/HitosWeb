271426503afbb739dcd20b99a4b6884a04a6d0bb
yanyuhito <h810126@nehs.tc.edu.tw>
2022-04-12 21:49:00 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,21 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/HitosWeb/'
     : '/'
+"transpileDependencies": [
+    "vuetify"
+  ],
+
+
+
+  pluginOptions: {
+    prerenderSpa: {
+
+      registry: undefined,
+      renderRoutes: [
+        '/', '/daily', '/my_family'],
+      useRenderEvent: true,
+      headless: true,
+      onlyProduction: true
+    }
+  }
 }
